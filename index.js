4158801a454efb1e3c3b9c38117e63804f49089a
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-04-29 17:33:48 +0200

    modify package.json

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+// Import routes
 const express = require("express");
+
+
+const PORT = 3000;
+
 const app = express();
 
+app.use('/', (req, res) => {
+  res.send('Hola Alfonsete');
+});
+
+// Up server
 app.listen(3000, () => {
   console.log(`Listening on http://localhost:3000/`)
 });
